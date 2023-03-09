e28b5914d6c7d7f343cf06708719468d7c1aa9e9
akashdoddmani <akashdoddmani115@gmail.com>
2023-03-09 11:10:50 +0530

    first commit

--- App.js
+++ App.js
@@ -4,9 +4,11 @@ import { NavigationContainer } from "@react-navigation/native";
 import 'react-native-gesture-handler';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import AboutStack from "./routes/aboutStack";
+import registerNNPushToken from 'native-notify';
 
 const Drawer=createDrawerNavigator();
 export default function App() {
+  registerNNPushToken(6653, 'Z7FwYfgBvidavdqtbGruga');
   return (
     <NavigationContainer>
       <Drawer.Navigator screenOptions={{drawerLabelStyle:{color:'#fff'}, headerShown:false,drawerStyle:{backgroundColor:'#181818',paddingTop:50}}}>
